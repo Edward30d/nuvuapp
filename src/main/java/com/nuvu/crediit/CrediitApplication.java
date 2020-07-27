afcd43cce8c5b6885c44f748400a1fff9c531f14
Edward30d <hellblazer30d@gmail.com>
2020-07-27 00:17:58 -0500

    Configuracion de consulta

--- src/main/java/com/nuvu/crediit/CrediitApplication.java
+++ src/main/java/com/nuvu/crediit/CrediitApplication.java
@@ -2,6 +2,7 @@ package com.nuvu.crediit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class CrediitApplication {
